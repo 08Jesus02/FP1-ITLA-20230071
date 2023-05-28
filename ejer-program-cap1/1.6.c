11fccaca9170db3e491d0d0599c5836ee999ba7e
Angel J. Brito <angeldejesus.bg2005@gmail.com>
2023-05-27 23:04:38 -0400

    Prueba

--- ejer-program-cap1/1.6.c
+++ ejer-program-cap1/1.6.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /* Aplicación de operadores. */
-int main(void) {
+int main(void)
+{
     int i = 15, j, k, l;
     j = (15 > i--) > (14 < ++i);
     printf("\nEl valor de j es: %d", j);
@@ -9,6 +10,7 @@ int main(void) {
     k = !('b' != 'd') > (!i - 1);
     printf("\nEl valor de k es: %d", k);
 
-    l = (! (34 > (70 % 2))) || 0;
+    l = (!(34 > (70 % 2))) || 0;
     printf("\nEl valor de l es: %d", l);
+    return 0;
 }
