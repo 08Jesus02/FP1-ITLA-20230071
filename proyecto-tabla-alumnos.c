7ca70d37c71414d3b103b711fb5cb80cc1fe9288
Angel J. Brito <angeldejesus.bg2005@gmail.com>
2023-07-08 19:30:25 -0400

    proyecto alumnos

--- proyecto-tabla-alumnos.c
+++ proyecto-tabla-alumnos.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+struct Alumno
 {
-    char nAlumno[50] = "";
-    char materia[50] = "";
-    float p1 = 0.0;
-    float p2 = 0.0;
-    float p3 = 0.0;
-    float promedio = 0.0;
+    char nAlumno[50];
+    char materia[50];
+    float p1;
+    float p2;
+    float p3;
+    float promedio;
+};
+
+int agregarAlumno(struct Alumno alumnos[], int *numAlumnos)
+{
+    struct Alumno nuevoAlumno;
 
     FILE *ar;
     ar = fopen("arc8.txt", "a");
 
     printf("\nIngresa Nombre Alumno: ");
-    gets(nAlumno);
-    // scanf("%s",&nAlumno);
+    // gets(nAlumno);
+    scanf("%s", nuevoAlumno.nAlumno);
 
     printf("\nIngresa Nombre Materia: ");
     scanf("%s", &materia);
@@ -30,9 +35,9 @@ int main()
     scanf("%f", &p3);
 
     promedio = (p1 + p2 + p3) / 3.0;
-    printf("|MATERIA|\t|ALUMNO|\t|P1|\t|P2|\t|P3|\t|PROMEDIO|");
-    printf("\n%s\t%s\t\t%0.2f\t%0.2f\t%0.2f\t%0.2f", materia, nAlumno, p1, p2, p3, promedio);
-
+    printf("|MATERIA|\t|ALUMNO|\\t|P1|\t|P2|\t|P3|\t|PROMEDIO|");
+    printf("\n%s\t\t%s\t%0.2f\t%0.2f\t%0.2f\t%0.2f", materia, nAlumno, p1, p2, p3, promedio);
     fprintf(ar, "\n%s\t%s\t%0.2f\t%0.2f\t%0.2f\t%0.2f", materia, nAlumno, p1, p2, p3, promedio);
+
     return 0;
 }
